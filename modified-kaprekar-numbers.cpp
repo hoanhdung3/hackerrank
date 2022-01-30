6348f08f11015d721995245dfb40e85baa6c4418
Ho Anh Dung <84617443+hoanhdung3@users.noreply.github.com>
2022-01-30 14:46:06 +0700

    Create modified-kaprekar-numbers.cpp

---
+++ modified-kaprekar-numbers.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'kaprekarNumbers' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER p
+ *  2. INTEGER q
+ */
+
+void kaprekarNumbers(int p, int q) {
+    int check=0;
+    for(long i=p;i<=q;i++){
+        long int a = i*i;
+        long int d = pow(10,to_string(i).length());
+        if(a/d+a%d == i){
+            cout<<i<<" ";
+            check=1;  
+        }
+    }
+    if(check==0) cout<<"INVALID RANGE";
+}
+
+int main()
+{
+    string p_temp;
+    getline(cin, p_temp);
+
+    int p = stoi(ltrim(rtrim(p_temp)));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    kaprekarNumbers(p, q);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
