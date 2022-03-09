4fee1c437eb294f32cda0d83a2da3c33cb5bf6cb
Ho Anh Dung <84617443+hoanhdung3@users.noreply.github.com>
2022-03-09 15:51:27 +0700

    Create anagram.cpp

---
+++ cpp/anagram.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'anagram' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int anagram(string s) {
+    if(s.length()%2!=0) return -1;
+    string d;
+    string a = s.substr(0,s.length()/2);
+    string b = s.substr(s.length()/2, s.length()/2);
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+    
+    set_difference(a.begin(),a.end(),b.begin(),b.end(),back_inserter(d));
+    return d.length();
+}
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = anagram(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
