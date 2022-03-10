1c7f9280e0bb5e01c2c7837aecbaadaa22bc84ed
Ho Anh Dung <84617443+hoanhdung3@users.noreply.github.com>
2022-03-10 17:05:41 +0700

    Create game-of-thrones-I.cpp

---
+++ cpp/game-of-thrones-I.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'gameOfThrones' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string gameOfThrones(string s) {
+    array<int,26> arr = {};
+    int c = 0;
+    for(auto i : s) arr[i-97]++;
+    for(int i=0;i<26;i++){
+        if(arr[i]!=0 && arr[i]%2!=0) c++;
+    }
+    return (c<2) ? "YES" : "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = gameOfThrones(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
